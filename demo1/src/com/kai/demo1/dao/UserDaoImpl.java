423636f13394132ce3b36340fe53fdbae049c945
kai.lv <kai@163.com>
2021-11-30 21:09:38 +0800

    AOP

---
+++ demo1/src/com/kai/demo1/dao/UserDaoImpl.java
@@ -0,0 +1,14 @@
+package com.kai.demo1.dao;
+
+/**
+ * @description:
+ * @author: kai.lv
+ * @date: 2021/11/29
+ **/
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void update() {
+        System.out.println("userDaoImpl.update()...");
+    }
+}
